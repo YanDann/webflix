e6420a0b1c376a518d0f33dd1188d01d53a8dfb6
Julien Michon <mcn.julien@gmail.com>
2023-07-19 10:31:33 +0200

    delete storage

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers;
 use App\Models\Category;
 use App\Models\Movie;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Storage;
 
 class MovieController extends Controller
 {
@@ -77,6 +79,8 @@ class MovieController extends Controller
 
     public function edit(Movie $movie)
     {
+        Gate::allowIf($movie->user_id === Auth::user()->id);
+
         return view('movies.edit', [
             'categories' => Category::all(),
             'movie' => $movie,
@@ -96,6 +100,12 @@ class MovieController extends Controller
             'released_at' => 'nullable|date',
             'category' => 'required|exists:categories,id',
         ]);
+
+        // Remplacer l'image s'il y en a une
+        if ($request->hasFile('cover')) {
+            Storage::delete(str($movie->cover)->remove('/storage/'));
+            $validated['cover'] = '/storage/'.$request->file('cover')->store('movies');
+        }
 
         $validated['category_id'] = $validated['category']; // Fix column name
         unset($validated['category']);
