f73a09f8ef1759782c7694866c97936978acf8b8
Julien Michon <mcn.julien@gmail.com>
2023-07-20 12:29:02 +0200

    front create + edit actor

--- resources/views/actors/create.blade.php
+++ resources/views/actors/create.blade.php
@@ -1,33 +1,37 @@
 @extends('layouts.app')
 
 @section('content')
-    <form action="" method="post" enctype="multipart/form-data">
-        @csrf
-        
-        <div>
-            <label for="name">Nom</label>
-            <input type="text" name="name" id="name" value="{{ old('name') }}">
-            @error('name')
-                <div> {{ $message }} </div>
-            @enderror
-        </div>
+    <div class="d-flex align-items-center justify-content-center">
+        <form class="w-25 px-3" action="" method="post" enctype="multipart/form-data">
+            @csrf
 
-        <div>
-            <label for="avatar">Avatar</label>
-            <input type="file" name="avatar" id="avatar">
-            @error('avatar')
-                <div> {{ $message }} </div>
-            @enderror
-        </div>
+            <div class="mb-3">
+                <label class="form-label" for="name">Nom</label>
+                <input class="form-control" type="text" name="name" id="name" value="{{ old('name') }}">
+                @error('name')
+                    <div> {{ $message }} </div>
+                @enderror
+            </div>
 
-        <div>
-            <label for="birthday">Date de naissance</label>
-            <input type="date" name="birthday" id="birthday">
-            @error('birthday')
-                <div> {{ $message }} </div>
-            @enderror
-        </div>
+            <div class="mb-3">
+                <label class="form-label" for="avatar">Avatar</label>
+                <input class="form-control" type="file" name="avatar" id="avatar">
+                @error('avatar')
+                    <div> {{ $message }} </div>
+                @enderror
+            </div>
 
-        <button>Ajouter</button>
-    </form>
+            <div class="mb-3">
+                <label class="form-label" for="birthday">Date de naissance</label>
+                <input class="form-control" type="date" name="birthday" id="birthday">
+                @error('birthday')
+                    <div> {{ $message }} </div>
+                @enderror
+            </div>
+
+            <div class="text-center">
+                <button class="btn btn-dark my-3">Ajouter</button>
+            </div>
+        </form>
+    </div>
 @endsection
--- resources/views/actors/edit.blade.php
+++ resources/views/actors/edit.blade.php
@@ -1,39 +1,51 @@
 @extends('layouts.app')
 
 @section('content')
-    <form action="" method="post" enctype="multipart/form-data">
-        @csrf
-        @method('put')
-        
-        <div>
-            <label for="name">Nom</label>
-            <input type="text" name="name" id="name" value="{{ old('name', $actor->name) }}">
-            @error('name')
-                <div> {{ $message }} </div>
-            @enderror
+    <h1 class="text-center mb-5">Modifier l'acteur : {{ $actor->name }} </h1>
+
+    <div class="d-flex align-items-center justify-content-center">
+        <div class="align-items-center px-3">
+            <img width="300" src="{{ $actor->avatar }}" alt="{{ $actor->name }}">
         </div>
 
-        <div>
-            <label for="avatar">Avatar</label>
-            <input type="file" name="avatar" id="avatar" value="{{ $actor->avatar }}">
-            @error('avatar')
-                <div> {{ $message }} </div>
-            @enderror
-        </div>
+        <form class="w-25 px-3" action="" method="post" enctype="multipart/form-data">
+            @csrf
+            @method('put')
 
-        <div>
-            <label for="birthday">Date de naissance</label>
-            @if ($actor->birthday != null)
-            <input type="date" name="birthday" id="birthday" value="{{ old('birthday', $actor->birthday->format('Y-m-d')) }}">
-            @endif
-            @if ($actor->birthday == null)
-            <input type="date" name="birthday" id="birthday" value="">
-            @endif
-            @error('birthday')
-                <div> {{ $message }} </div>
-            @enderror
-        </div>
+            <div class="mb-3">
+                <label class="form-label" for="name">Nom</label>
+                <input class="form-control" type="text" name="name" id="name"
+                    value="{{ old('name', $actor->name) }}">
+                @error('name')
+                    <div> {{ $message }} </div>
+                @enderror
+            </div>
 
-        <button>Modifier</button>
-    </form>
+            <div class="mb-3">
+                <label class="form-label" for="avatar">Avatar</label>
+                <input class="form-control" type="file" name="avatar" id="avatar" value="{{ $actor->avatar }}">
+                @error('avatar')
+                    <div> {{ $message }} </div>
+                @enderror
+            </div>
+
+            <div class="mb-3">
+                <label class="form-label" for="birthday">Date de naissance</label>
+                @if ($actor->birthday != null)
+                    <input class="form-control" type="date" name="birthday" id="birthday"
+                        value="{{ old('birthday', $actor->birthday->format('Y-m-d')) }}">
+                @endif
+                @if ($actor->birthday == null)
+                    <input class="form-control" type="date" name="birthday" id="birthday" value="">
+                @endif
+                @error('birthday')
+                    <div> {{ $message }} </div>
+                @enderror
+            </div>
+
+            <div class="text-center">
+                <button class="btn btn-dark my-3">Modifier</button>
+            </div>
+        </form>
+    </div>
 @endsection
