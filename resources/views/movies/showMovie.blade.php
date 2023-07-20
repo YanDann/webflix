bb06b74b3c544ab6fbc2ad694e647ec9cf83e616
Julien Michon <mcn.julien@gmail.com>
2023-07-20 10:51:10 +0200

    change caterogy-ID to category-name in showMovie

--- resources/views/movies/showMovie.blade.php
+++ resources/views/movies/showMovie.blade.php
@@ -8,10 +8,35 @@
 @section('content')
     <div>
         <h1> {{ $movie->title }} </h1>
+
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            Voir Bande-annonce
+        </button>
+
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="exampleModalLabel">Bande annonce</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="embed-responsive embed-responsive-16by9">
+                            <iframe class="embed-responsive-item" width="560" height="315" src="https://www.youtube.com/embed/{{ $movie->youtube }}"
+                                allowfullscreen></iframe>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
         <p> {{ $movie->synopsys }} </p>
         <p>Durée : {{ $movie->duration }} </p>
         <p>Sortie : {{ $movie->released_at }} </p>
-        <p>Catégorie : {{ $movie->category_id }} </p>
+        <p>Catégorie : {{ $movie->category->name }} </p>
     </div>
 
     <div>
